94dad2b3f53e5ebbd65159bd51e5ae3e660766eb
Peramanathan Sathyamoorthy <sathyam.peram@gmail.com>
2022-10-07 08:55:36 +0200

    feature: Add asking user consent flow for microphone

--- app/components/recorder.tsx
+++ app/components/recorder.tsx
@@ -14,6 +14,7 @@ if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
 
 type RecorderContext = {
   microphones: Array<MediaDeviceInfo>;
+  microphonePermission: 'granted' | 'prompt' | 'denied';
   selectedMicrophone: MediaDeviceInfo | null;
   recordedAudio: Blob | null;
   audioDowdoadUrl: string;
@@ -40,6 +41,7 @@ const recorderMachine = createMachine<RecorderContext>(
     predictableActionArguments: true,
     context: {
       microphones: [],
+      microphonePermission: 'prompt',
       selectedMicrophone: null,
       recordedAudio: null,
       audioDowdoadUrl: '',
@@ -64,6 +66,19 @@ const recorderMachine = createMachine<RecorderContext>(
             actions: 'selectMicrophone',
           },
           START_RECORDING: 'recording',
+          GET_MICROPHONE_CONSENT: {
+            target: 'getting_microphone_consent',
+          },
+        },
+      },
+      getting_microphone_consent: {
+        invoke: {
+          src: 'getMicrophoneConsent',
+          id: 'microphoneConsent',
+          onDone: {
+            target: 'reading_microphones',
+            actions: 'updateMicrophoneConsent',
+          },
         },
       },
       recording: {
@@ -79,6 +94,9 @@ const recorderMachine = createMachine<RecorderContext>(
               STOP: {
                 target: 'stopped',
                 actions: sendTo('stop', { to: 'mediaRecorder' }),
+              },
+              MICROPHONE_CONSENT_DENIED: {
+                target: '#voiceRecorder.ready_to_record',
               },
             },
           },
@@ -104,8 +122,9 @@ const recorderMachine = createMachine<RecorderContext>(
   {
     actions: {
       assignMicrophones: assign({
-        microphones: (_, event) => event.data,
-        selectedMicrophone: (_, event) => event.data?.[0],
+        microphones: (_, event) => event.data.audioInputDevices,
+        selectedMicrophone: (_, event) => event.data.audioInputDevices?.[0],
+        microphonePermission: (_, event) => event.data.permission,
       }),
       selectMicrophone: assign({
         selectedMicrophone: (_, event) => event.microphone,
@@ -114,12 +133,34 @@ const recorderMachine = createMachine<RecorderContext>(
         recordedAudio: (_, event) => event.recordedAudio,
         audioDowdoadUrl: (_, event) => event.audioDowdoadUrl,
       }),
+      updateMicrophoneConsent: assign({
+        microphonePermission: (_, event) => event.data.consent,
+      }),
     },
     services: {
       getAvailableMicrophones: async function getAllMicroPhobes() {
         const allDevices = await navigator.mediaDevices.enumerateDevices();
+        const audioInputDevices = allDevices.filter(
+          (device) => device.kind === 'audioinput'
+        );
 
-        return allDevices.filter((device) => device.kind === 'audioinput');
+        const microphonePermission = await navigator.permissions.query({
+          name: 'microphone',
+        });
+
+        return { audioInputDevices, permission: microphonePermission.state };
+      },
+      getMicrophoneConsent: async function getMicrophoneConsent() {
+        try {
+          await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: false,
+          });
+
+          return { consent: 'granted' };
+        } catch {
+          return { consent: 'denied' };
+        }
       },
       startRecording: (context) => (sendBack, onReceive) => {
         let mediaRecorder: MediaRecorder;
@@ -147,10 +188,19 @@ const recorderMachine = createMachine<RecorderContext>(
         async function startVoiceRecording() {
           const deviceId = context.selectedMicrophone?.deviceId;
           const audio = deviceId ? { deviceId: { exact: deviceId } } : true;
-          const mediaStream = await navigator.mediaDevices.getUserMedia({
-            audio,
-            video: false,
-          });
+          let mediaStream;
+          try {
+            mediaStream = await navigator.mediaDevices.getUserMedia({
+              audio,
+              video: false,
+            });
+          } catch (error) {
+            sendBack({
+              type: 'MICROPHONE_CONSENT_DENIED',
+            });
+            return;
+          }
+
           mediaRecorder = new MediaRecorder(mediaStream, {
             mimeType: 'audio/webm',
           });
@@ -196,12 +246,75 @@ export default function Recorder(props: Props) {
   });
   const { context } = state;
 
+  const renderRecorder = () => {
+    if (context.microphonePermission === 'denied') {
+      return null;
+    }
+
+    return (
+      <div className="flex flex-col gap-4">
+        <div className="py-2 flex items-center gap-4">
+          {state.matches('ready_to_record') ? (
+            <button
+              className="p-2 text-white rounded-full bg-pink-700 hover:bg-pink-500"
+              onClick={() => send({ type: 'START_RECORDING' })}
+            >
+              Record
+            </button>
+          ) : null}
+          {state.matches('recording') && !state.matches('recording.stopped') ? (
+            <>
+              <button
+                className="p-2 text-white rounded-full bg-pink-700 hover:bg-pink-500"
+                onClick={() => send({ type: 'STOP' })}
+              >
+                Stop
+              </button>
+              <span>Recording...</span>
+            </>
+          ) : null}
+          {state.matches('recording.stopped') ? (
+            <>
+              <a
+                role="button"
+                className="inline-block text-white p-2 rounded-full bg-pink-700 hover:bg-pink-500"
+                href={context.audioDowdoadUrl}
+                download={`${audioFileName}.webm` || 'recorded.webm'}
+              >
+                Download
+              </a>
+              <button
+                className="p-2 text-white rounded-full bg-pink-700 hover:bg-pink-500"
+                onClick={() => send({ type: 'RESTART' })}
+              >
+                Re-record
+              </button>
+            </>
+          ) : null}
+        </div>
+        <div>
+          {state.matches('recording.stopped') ? (
+            <audio
+              className="w-[250px]"
+              controls
+              src={context.audioDowdoadUrl}
+            />
+          ) : null}
+        </div>
+      </div>
+    );
+  };
+
   return (
     <>
       <details className="hidden sm:block p-4 shadow-2xl rounded-lg dark:bg-gray-800 open:dark:shadow-glowing">
         <summary>Want to record and download in your voice?</summary>
 
-        {context.microphones.length ? (
+        {context.microphonePermission === 'denied' ? (
+          <div>Microphone access is denied</div>
+        ) : null}
+
+        {context.microphonePermission === 'granted' ? (
           <select
             name="microphones"
             className="p-4 text-black dark:text-white dark:bg-gray-600 border-rose-700 rounded-lg"
@@ -217,58 +330,15 @@ export default function Recorder(props: Props) {
             ))}
           </select>
         ) : null}
-
-        <div className="flex flex-col gap-4">
-          <div className="py-2 flex items-center gap-4">
-            {state.matches('ready_to_record') ? (
-              <button
-                className="p-2 text-white rounded-full bg-pink-700 hover:bg-pink-500"
-                onClick={() => send({ type: 'START_RECORDING' })}
-              >
-                Record
-              </button>
-            ) : null}
-            {state.matches('recording') &&
-            !state.matches('recording.stopped') ? (
-              <>
-                <button
-                  className="p-2 text-white rounded-full bg-pink-700 hover:bg-pink-500"
-                  onClick={() => send({ type: 'STOP' })}
-                >
-                  Stop
-                </button>
-                <span>Recording...</span>
-              </>
-            ) : null}
-            {state.matches('recording.stopped') ? (
-              <>
-                <a
-                  role="button"
-                  className="inline-block text-white p-2 rounded-full bg-pink-700 hover:bg-pink-500"
-                  href={context.audioDowdoadUrl}
-                  download={`${audioFileName}.webm` || 'recorded.webm'}
-                >
-                  Download
-                </a>
-                <button
-                  className="p-2 text-white rounded-full bg-pink-700 hover:bg-pink-500"
-                  onClick={() => send({ type: 'RESTART' })}
-                >
-                  Re-record
-                </button>
-              </>
-            ) : null}
-          </div>
-          <div>
-            {state.matches('recording.stopped') ? (
-              <audio
-                className="w-[250px]"
-                controls
-                src={context.audioDowdoadUrl}
-              />
-            ) : null}
-          </div>
-        </div>
+        {context.microphonePermission === 'prompt' ? (
+          <button
+            className="p-2 text-white rounded-full bg-pink-700 hover:bg-pink-500"
+            onClick={() => send({ type: 'GET_MICROPHONE_CONSENT' })}
+          >
+            Load All Microphones
+          </button>
+        ) : null}
+        {renderRecorder()}
       </details>
     </>
   );
